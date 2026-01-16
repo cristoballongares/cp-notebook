abfe849c4da7f9fda35a4b3c07e96a63634d94db
cristoballongares <78714887+cristoballongares@users.noreply.github.com>
2026-01-16 00:00:59 -0600

    aun sin resolver xD

--- test.cpp
+++ test.cpp
@@ -22,30 +22,117 @@ struct segment{
 // Recordemos que esta estrategia solo la usaremos en caso de no cumplir ningun caso util, por lo tanto
 // Empezaremos con aquellos mas cercanos a [0,m]
 
+// Usar busqueda binaria en la respuesta, la complejidad sera O(nlogn)
+// La idea es simple:p, es posible construir un segmento de k segmentos?
+// Para ello, tenemos que ordenar el conjutno de segmentos, sencillo
+// iterar por los n segmentos
+
+// Pero, y si hacemos n^2, doble iteracion!
+// Evaluar cada caso, que pasa si empezamos desde 0 b, es posible construir el segmento con k segmentos?
+// Si, listo, facil.
+// No?, se intento, pasamos con el siguiente b_i
+
+// Primero probemos la primer idea
+
+bool check(vector<segment> segs, int k,int m){
+    int i,j;
+    int count = 0;
+    for(i=0;i<segs.size();i++){
+        segment s = segs[i];
+        int b = s.l;
+        
+        if(b==0 && s.r==0) continue; // evitamos 0,0
+
+        if(b<0) b=0;
+        count = 1; 
+        for(j=0;j<segs.size();j++){
+            if(i==j) continue; // No sirve comparar los mismos segmentos
+            segment s2 = segs[j];
+            int c = s2.l, d = s2.r;
+            if(c<0) c=0;
+            // Encontramos un segmento que empieza en b!, checamos su c!
+            if(b==c && d!=c){
+                count++;
+            }
+            b = c; // Actualizamos el punto final
+
+            if(d==m) if(count==k) return true; // Es posible construir un segmento con k segmentos:)
+
+        }
+    }
+
+    return false;
+}
+
+
 void solve(){
     int m; cin>>m;
 
     // En un inicio, todos los segmentos parecen ser opciones
     vector<segment> segments;
     int a,b;
-    while(cin>>a>>b){
+    bool ok=1;
+    while(cin>>a>>b && ok){
         // Lo encontramos de una
         if(a==0 && b==m){
-            cout<<0<<' '<<m;
+            cout<<1<<'\n'<<0<<' '<<m;
             return ;
         }
         
         if(a<=0 && b>=m){
-            cout<<a<<' '<<b;
+            cout<<1<<'\n'<<a<<' '<<b;
             return ;
         }
 
         segment s;
         s.l = a; s.r = b;
         if(a<=m)segments.push_back(s);
-        if(a==0 &&b==0) break;
+
+        sort(segments.begin(), segments.end(), [&](auto a, auto b){
+            int lA = a.l; int lB = b.l;
+            if(lA==lB) return a.r>b.r;
+            return lA<lB;
+
+        });
+        if(a==0&&b==0) ok = 0;
     }
+
+    // for(segment s:segments) cout<<s.l<<' '<<s.r<<'\n';
+    int i;
+    stack<segment> s;
+    s.push(segments[0]);
+    int A = segments[0].l, B = segments[0].r;
+    
+    for(i=1;i<segments.size();i++){
+
+        int l=segments[i].l, r = segments[i].r;
+
+        while(s.size()>1 && (l==s.top().l&& r>s.top().r)){
+            cout<<"\tsacando: "<<s.top().l<<' '<<s.top().r<<'\n';
+            s.pop();
+        }
+
+
+        if(r>B) s.push(segments[i]);
+        if(s.top().r==m) break;
+
+    }
+    if(s.top().r!=m){ cout<<-1; return;}
+    
+    cout<<s.size()<<'\n';
+    stack<segment> sol;
+    while(!s.empty()){
+        sol.push(s.top());
+        s.pop();
+    }
+    while(!sol.empty()){
+        cout<<sol.top().l<<' '<<sol.top().r<<'\n';
+        sol.pop();
+    }
+
+
 }
+
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
@@ -54,7 +141,7 @@ int main(){
 
     while(t--){
         solve();
-        cout<<'\n';
+        cout<<"\n\n";
     }
 
     return 0;
