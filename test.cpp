987bbd437b3ef2af5484f3647263fc572ce39801
cristoballongares <78714887+cristoballongares@users.noreply.github.com>
2026-02-20 21:50:08 -0600

    asd

--- test.cpp
+++ test.cpp
@@ -3,47 +3,15 @@ using namespace std;
 using ll = long long;
 using lli = long long int;
 
-int n, m;
-bool bad[21][21];
-int total = 0;
-
-vector<int> pizza_actual;
-
-void buscar(int id) {
-    if(id>n){
-        total++;
-        return;
-    }
-    buscar(id+1);
-
-    bool ok = true;
-    for(int ing:pizza_actual) {
-        if(bad[id][ing]) {
-            ok=false;
-            break;
-        }
-    }
-
-    if(ok){
-        pizza_actual.push_back(id); // metemos
-        buscar(id+1); // seguimos buscando con el siguiente
-        pizza_actual.pop_back(); // lo sacamos para probar otras
-    }
-}
-
 void solve(){
-    cin>>n>>m;
-    int t = m;
-    while(t--){
-        int x,y;cin>>x>>y;
-        bad[x][y] = true;
-        bad[y][x] = true;
-    }
+    
+    int n; cin>>n;
+    vector<int>a(n);
+    for(int &x:a)cin>>x;
 
-    buscar(1);
-    cout<<total;
+    
+
 }
-
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(nullptr);
